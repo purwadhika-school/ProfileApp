dedd94fdf02ee6f897b72a207939098e26e9da34
yogieputra8 <yogie.putra8@gmail.com>
2018-11-29 22:14:11 +0700

    remove unused lines

--- App.js
+++ App.js
@@ -10,14 +10,13 @@ import InputFormName from "./src/inputs/InputFormName"
 import InputFormGender from "./src/inputs/InputFormGender"
 import InputFormPhone from "./src/inputs/InputFormPhone"
 import InputFormAddress from "./src/inputs/InputFormAddress"
+import Main from "./src/Main"
+import Result from "./src/Result"
 
 class App extends Component {
-  
+  render() {
+    return <Main />
+  }
 }
 
-
-
 export default App
-
-// 1. Bikin 4-pairs inputan
-// 2.
